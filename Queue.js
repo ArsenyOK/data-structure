354180b35cec4d1a12c7d3b50ce91591892e6dd9
ArsenyOK <arsen.pilipenko2014@gmail.com>
2022-12-18 12:45:35 +0200

    Created class for Queue

---
+++ Queue.js
@@ -0,0 +1,32 @@
+// enqueue: enter to the queue, add an element to the end
+// dequeue: leave the queue, remove the first element and return it
+// front | peek: get the first element
+// isEmpty: check, is the queue empty?
+// length: get the quantity of the queue
+
+class Queue {
+  constructor() {
+    this.elements = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(element) {
+    this.elements[this.tail] = element;
+    this.tail++;
+  }
+  dequeue() {
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
+  }
+  peek() {
+    return this.elements[this.head];
+  }
+  get length() {
+    return this.tail - this.head;
+  }
+  get isEmpty() {
+    return this.length === 0;
+  }
+}
